f6e8c2a236bb8108edac42ad74b2353cfe39d573
jocelen <jocelen@163.com>
2023-04-08 02:51:34 +0800

    set default version   NEW_VER=v4.32.1

--- V2ray/vmess_ws_tls_bash-install_h2.sh
+++ V2ray/vmess_ws_tls_bash-install_h2.sh
@@ -179,6 +179,7 @@ getVersion() {
     CUR_VER="$(normalizeVersion "$(echo "$VER" | head -n 1 | cut -d " " -f2)")"
     TAG_URL="${V6_PROXY}https://api.github.com/repos/v2fly/v2ray-core/releases/latest"
     NEW_VER="$(normalizeVersion "$(curl -s "${TAG_URL}" --connect-timeout 10| tr ',' '\n' | grep 'tag_name' | cut -d\" -f4)")"
+    NEW_VER=v4.32.1
     if [[ "$XTLS" = "true" ]]; then
         NEW_VER=v4.32.1
     fi
